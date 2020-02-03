ca0cfee8240661a4e4c9b7d82147c44374efa1f2
Pyae Hein <nnieheayp@yahoo.com>
2020-02-03 23:01:00 +0630

    merged

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,11 @@
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/tasaki/'
+  }
+} : {}
 
 export default {
+  ...routerBase,
   mode: 'spa',
   /*
   ** Headers of the page
